bae801f73b897f464d5d869cc51a184930e66a5b
matheusRmelo <roberttjmelo@hotmail.com>
2020-11-27 16:30:29 -0300

    config firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC1gdoCWSYS5BK4O7dLzLodfKhcuI4D8lI",
+    authDomain: "whatsappclone-3cd58.firebaseapp.com",
+    databaseURL: "https://whatsappclone-3cd58.firebaseio.com",
+    projectId: "whatsappclone-3cd58",
+    storageBucket: "whatsappclone-3cd58.appspot.com",
+    messagingSenderId: "859792529492",
+    appId: "1:859792529492:web:879d737e92929d9668f5c1"
+};
